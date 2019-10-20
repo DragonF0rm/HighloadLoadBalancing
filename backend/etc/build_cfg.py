a38276d959b133ad85894b039b6d5cdb65fcabdc
Maxim Uymin <m.uymin@corp.mail.ru>
2019-10-21 02:33:47 +0300

    Config building script fixed

--- backend/etc/build_cfg.py
+++ backend/etc/build_cfg.py
@@ -3,12 +3,9 @@ import yaml
 import sys
 from jinja2 import Environment, FileSystemLoader
 
-node_id = sys.argv[1]
-if __name__ == "__main__":
- # yaml = YAML()
- config_data = yaml.load(open('./envoy.yaml.j2'))
- print(config_data)
+basepath = os.path.abspath(os.path.dirname(__file__))
+config_data = yaml.load(open(basepath + '/envoy.yaml.j2'))
 # Load templates file from templtes folder
- env = Environment(loader = FileSystemLoader('./templates'), trim_blocks=True, lstrip_blocks=True)
- template = env.get_template('template.txt')
- print(template.render(config_data))
+env = Environment(loader = FileSystemLoader('./templates'), trim_blocks=True, lstrip_blocks=True)
+template = env.get_template('template.txt')
+print(template.render(config_data))
