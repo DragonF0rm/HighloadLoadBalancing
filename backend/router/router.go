d9f1a17b84a99fdd0679e50effb25597e4f5c793
Maxim Uymin <m.uymin@corp.mail.ru>
2019-10-26 18:43:24 +0300

    Backend router fixed

--- backend/router/router.go
+++ backend/router/router.go
@@ -3,14 +3,13 @@ package router
 import (
 	"LoadBalancingBackend/metric"
 	"github.com/gorilla/mux"
-	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"net/http"
 )
 
 func GetRouter() *mux.Router {
 	router := mux.NewRouter()
 	router.Use(AccessLogMiddleware)
-	router.Handle("/metrics", promhttp.Handler())
+	router.Handle("/stats/prometheus", metric.ExposeMetrics())
 
 	api := router.PathPrefix("/api").Subrouter()
 	api.HandleFunc("/liveness-probe", LivenessProbeHandler).Methods(http.MethodGet)
@@ -21,7 +20,6 @@ func GetRouter() *mux.Router {
 	admin.HandleFunc("/overload", OverloadHandler).Methods(http.MethodGet)
 	admin.HandleFunc("/error", ErrorHandler).Methods(http.MethodGet)
 	admin.HandleFunc("/reboot", RebootHandler).Methods(http.MethodGet)
-	admin.Handle("/stats/prometheus", metric.ExposeMetrics())
 
 	return router
 }
